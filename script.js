8e0bfc47267ed18b65d5c30d5924e23a7385eec7
towaldi <tobiaswalddoerfer@gmail.com>
2023-03-10 22:11:42 +0100

    basic css structure + field id

---
+++ script.js
@@ -0,0 +1,9 @@
+// Array -> stores if palyers occupation sign was set 
+let squares = [];
+
+
+// Update squares
+function fillSquares(id) {      // Id -> fpr each square
+    squares[id] = 'circle';     // Square is filled with 'circle' or 'cross'
+    console.log(squares);
+}
